883b4ba5d0f762724340c15914d9c68242977cbc
jbcruz <35581373+juleskoderepo@users.noreply.github.com>
2018-08-15 15:26:44 -0500

    Implement CursorLoader

--- app/src/main/java/com/example/android/pets/CatalogActivity.java
+++ app/src/main/java/com/example/android/pets/CatalogActivity.java
@@ -15,10 +15,12 @@
  */
 package com.example.android.pets;
 
+import android.app.LoaderManager;
 import android.content.ContentValues;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.Loader;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -28,19 +30,20 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.android.pets.data.PetsContract.petsEntry;
-import com.example.android.pets.data.PetDbHelper;
 
 
 /**
  * Displays list of pets that were entered and stored in the app.
  */
-public class CatalogActivity extends AppCompatActivity {
+public class CatalogActivity extends AppCompatActivity
+        implements LoaderManager.LoaderCallbacks<Cursor> {
 
-    PetDbHelper mDbHelper;
+    PetCursorAdapter cursorAdapter;
+
+    private static final int PETAPP_LOADER_ID = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,44 +67,15 @@ public class CatalogActivity extends AppCompatActivity {
         View emptyView = findViewById(R.id.empty_view);
         petListView.setEmptyView(emptyView);
 
-        // Database helper that provides access to the database
-        mDbHelper = new PetDbHelper(this);
+        // Set up cursor adapter using cursor
+        cursorAdapter = new PetCursorAdapter(this, null, 0);
+        // Attach cursor adapter to ListView
+        petListView.setAdapter(cursorAdapter);
+
+        // Initialize the CursorLoader
+        getLoaderManager().initLoader(PETAPP_LOADER_ID, null, this);
 
     }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        displayDatabaseInfo();
-    }
-
-    /**
-     * Temporary helper method to display information in the onscreen TextView about the state of
-     * the pets database.
-     */
-    private void displayDatabaseInfo() {
-        // Declare and assign values for parameters needed for query method
-        String[] projection = null;
-        String selection = null;
-        String[] selectionArgs = null;
-        String sortOrder = null;
-
-        // Call the Content Resolver, that will call the PetsProvider to query the database
-        // and return a Cursor.
-        Cursor cursor = getContentResolver().query(petsEntry.CONTENT_URI,
-                projection, // columns to return
-                selection, // selection criteria, i.e. WHERE
-                selectionArgs, // values for selection criteria
-                sortOrder); // sort order for rows returned
-
-            // Find ListView to populate
-            ListView petListView = findViewById(R.id.list);
-            // Set up cursor adapter using cursor
-            PetCursorAdapter cursorAdapter = new PetCursorAdapter(this, cursor, 0);
-            // Attach cursor adapter to ListView
-            petListView.setAdapter(cursorAdapter);
-    }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -111,7 +85,6 @@ public class CatalogActivity extends AppCompatActivity {
         return true;
     }
 
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // User clicked on a menu option in the app bar overflow menu
@@ -120,7 +93,6 @@ public class CatalogActivity extends AppCompatActivity {
             case R.id.action_insert_dummy_data:
                 insertPet();
                 Toast.makeText(this, getString(R.string.pet_saved), Toast.LENGTH_LONG).show();
-                displayDatabaseInfo();
 
                 return true;
             // Respond to a click on the "Delete all entries" menu option
@@ -130,7 +102,6 @@ public class CatalogActivity extends AppCompatActivity {
                         String.valueOf(1), null);
                 Toast.makeText(this, getString(R.string.rows_deleted) + rowsDeleted,
                         Toast.LENGTH_LONG).show();
-                displayDatabaseInfo();
 
                 return true;
         }
@@ -154,4 +125,38 @@ public class CatalogActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public Loader<Cursor> onCreateLoader(int i /*Loader ID*/, Bundle bundle) {
+        // Declare and assign values for parameters needed for CursorLoader
+        String[] projection = {
+                petsEntry._ID,
+                petsEntry.COLUMN_PET_NAME,
+                petsEntry.COLUMN_PET_BREED};
+        String selection = null;
+        String[] selectionArgs = null;
+        String sortOrder = null;
+
+        // Return a CursorLoader that executes the ContentProvider's query method on a
+        // background thread
+        return new CursorLoader(this,
+                petsEntry.CONTENT_URI,
+                projection,
+                selection,
+                selectionArgs,
+                sortOrder);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        // Moves the query results into the adapter, causing the ListView fronting the
+        // adapter to re-display
+        cursorAdapter.swapCursor(cursor);
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        // Clear out adapter's reference to the Cursor to prevent memory leaks
+        cursorAdapter.swapCursor(null);
+    }
 }
